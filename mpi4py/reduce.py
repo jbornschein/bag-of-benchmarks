5da19a77736579ac2091a46f82fc7756fac6d398
Joerg Bornschein <bornschein@fias.uni-frankfurt.de>
2012-11-28 00:17:52 +0100

    Anhance mpi4py reduce benchmark

--- mpi4py/reduce.py
+++ mpi4py/reduce.py
@@ -12,12 +12,12 @@ import numpy as np
 from parutils import pprint
 
 sizes = [ 2**n for n in xrange(1,24) ]
-runs  = 20
+runs  = 50
 
 comm = MPI.COMM_WORLD
 
 
-pprint("Benchmarking reduce performance on %d parallel MPI processes..." % comm.size)
+pprint("Benchmarking Reduce performance on %d parallel MPI processes..." % comm.size)
 pprint()
 pprint("%15s | %12s | %12s" % 
     ("Size (bytes)", "Time (msec)", "Bandwidth (MiBytes/s)"))
@@ -27,11 +27,35 @@ for s in sizes:
     res = np.empty_like(data)
 
     comm.Barrier()
-    t0 = time()
+    t_min = np.inf
     for i in xrange(runs):
+        t0 = time()
         comm.Reduce( [data, MPI.DOUBLE], [res, MPI.DOUBLE] ) 
+        t = time()-t0
+        t_min = min(t, t_min)
     comm.Barrier()
-    t = (time()-t0) / runs
     
     pprint("%15d | %12.3f | %12.3f" % 
-        (data.nbytes, t*1000, data.nbytes/t/1024/1024) )
+        (data.nbytes, t_min*1000, data.nbytes/t_min/1024/1024) )
+
+pprint("Benchmarking AllReduce performance on %d parallel MPI processes..." % comm.size)
+pprint()
+pprint("%15s | %12s | %12s" % 
+    ("Size (bytes)", "Time (msec)", "Bandwidth (MiBytes/s)"))
+
+for s in sizes:
+    data = np.ones(s)
+    res = np.empty_like(data)
+
+    comm.Barrier()
+    t_min = np.inf
+    for i in xrange(runs):
+        t0 = time()
+        comm.Allreduce( [data, MPI.DOUBLE], [res, MPI.DOUBLE] ) 
+        t = time()-t0
+        t_min = min(t, t_min)
+    comm.Barrier()
+    
+    pprint("%15d | %12.3f | %12.3f" % 
+        (data.nbytes, t_min*1000, data.nbytes/t_min/1024/1024) )
+
